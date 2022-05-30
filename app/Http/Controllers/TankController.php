7b59d3cd8cb7298b8a78a4cd3918dd389fb2ee0b
kamil <kamil.nowik.priv@gmail.com>
2022-05-30 16:34:19 +0200

    zmiany zmiany

--- app/Http/Controllers/TankController.php
+++ app/Http/Controllers/TankController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use App\Models\CrewMember;
 use App\Models\Tank;
 use App\Services\WgApiService;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class TankController extends Controller
@@ -21,6 +24,7 @@ class TankController extends Controller
             $tank = new Tank();
             $tank->name = $vehicle['name'];
             $tank->wg_id = $vehicle['tank_id'];
+            $tank->image_link = $vehicle['images']['big_icon'];
             $tank->save();
 
             foreach ($vehicle['crew'] as $member) {
@@ -34,8 +38,21 @@ class TankController extends Controller
         return 'hehe';
     }
 
-    public function edit(Request $request)
+    /**
+     * @param Request $request
+     * @return Factory|View|Application
+     */
+    public function edit(Request $request): Factory|View|Application
     {
-       return view('welcome');
+        if ($request->selected) {
+            $selectedTank = Tank::where('id', $request->selected)->first()->load('crewMembers');
+        } else {
+            $selectedTank = false;
+        }
+
+        return view('welcome')
+            ->with('tanks', Tank::orderBy('name')->get())
+            ->with('selectedTank', $selectedTank)
+            ->with('skills', CrewMember::SKILLS);
     }
 }
