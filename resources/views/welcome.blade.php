a4c24b970c4a19af4abeebf7a3d6697630c0a1cc
kamil <kamil.nowik.priv@gmail.com>
2022-05-30 16:34:45 +0200

    zmiany zmiany

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,13 +8,43 @@
           integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 </head>
 <body>
-<select class="form-select" aria-label="Default select example">
-    @for()
-    <option selected>Open this select menu</option>
-    <option value="1">One</option>
-    <option value="2">Two</option>
-    <option value="3">Three</option>
-</select>
+<div class="container">
+    <div class="row" style="overflow:scroll; height:400px; margin-top: 75px; border:1px solid black">
+        @foreach($tanks as $tank)
+            <div class="col">
+                <a class="btn btn-default" role="button" href="{{route('index')}}/?selected={{$tank->id}}">
+                    <img src="{{$tank->image_link}}" alt="" style="height:75px"> {{$tank->name}}
+                </a>
+
+            </div>
+        @endforeach
+    </div>
+    <div class="row" style="margin-top: 75px; border:1px solid black">
+        @if($selectedTank)
+            <form>
+                <fieldset>
+                    <legend>PERKI DLA ZAŁOGI 1 PERKOWEJ</legend>
+
+                    @foreach($selectedTank->crewMembers as $crewMember )
+                        <div class="mb-3">
+                            <label for="disabledSelect" class="form-label">{{$crewMember->role_name}}</label>
+                            <select id="disabledSelect" class="form-select">
+                                <option>Disabled select</option>
+                            </select>
+                        </div>
+                    @endforeach
+
+
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </fieldset>
+            </form>
+
+
+        @else
+            Wybierz czołg
+        @endif
+    </div>
+</div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
         crossorigin="anonymous"></script>
