3e5996ffdc620a9e3e999604a321e0bc934ea5cd
kamil <kamil.nowik.priv@gmail.com>
2022-05-31 21:23:48 +0200

    zmiany zmiany

--- app/Models/Tank.php
+++ app/Models/Tank.php
@@ -4,17 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Tank extends Model
 {
     use HasFactory;
 
     /**
-     * @return HasMany
+     * @return belongsToMany
      */
-    public function crewMembers(): HasMany
+    public function crewMembers(): belongsToMany
     {
-        return $this->hasMany(TankCrewMember::class);
+        return $this->belongsToMany(
+            CrewMember::class,
+            'tank_crew_members',
+            'tank_wg_id',
+            'crew_member_role',
+            'wg_id',
+            'role');
     }
 }
