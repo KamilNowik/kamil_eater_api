1a112448dc9fdd090da5cace2bd7c46f530ea0bf
kamil <kamil.nowik.priv@gmail.com>
2022-06-11 01:54:19 +0200

    zmiany zmiany

--- resources/js/vue/components/TanksBrowser.vue
+++ resources/js/vue/components/TanksBrowser.vue
@@ -3,20 +3,16 @@
         <div class="container" style="height:5000px">
             <div class="row">
                 <div v-for="tank in tanks.data" class="col-xl-2 col-lg-4 col-6">
-                    <div>
-                        <a href="#"
-                           style="background-image: url(&quot;https://stefanisarie.photography/assets/images/flags/flag-china.png&quot;);">
-                            <img
-                                alt="" title=""
-                                :src="tank.image_link"
-                                class="mx-auto g-image"></a><a href="/62001/m41-d/" class="flex flex-col text-center">
-                        <p class="text-sm font-extrabold text-wg-text overflow-hidden overflow-ellipsis whitespace-nowrap text-wg-playable">
-                            {{tank.name}}
-                        </p>
-                        <p class="mb-5 text-xs text-wg-mutted">
-                            China tier VIII <span class="text-wg-altaccent">premium</span> vehicle
-                        </p>
-                    </a>
+                    <div class="tank-container">
+                        <a href="/62001/m41-d/" class="tank-a flex flex-col text-center">
+                            <img alt="" title="" :src="tank.image_link" class="mx-auto g-image">
+                            <p class="text-sm font-extrabold text-wg-text overflow-hidden overflow-ellipsis whitespace-nowrap text-wg-playable">
+                                {{ tank.name }}
+                            </p>
+                            <p class="mb-5 text-xs text-wg-mutted">
+                                {{tank.nation}} tier VIII <span v-if="tank.is_premium">premium</span> vehicle
+                            </p>
+                        </a>
                     </div>
                 </div>
             </div>
